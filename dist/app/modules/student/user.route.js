f2d97d68e98886d847dd6cb4074535f5ebb43c8b
Debos Das <debos.das.02@gmail.com>
2025-12-24 06:18:21 +0600

    solve issue

--- dist/app/modules/student/user.route.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserRoutes = void 0;
-var student_route_1 = require("./student.route");
-Object.defineProperty(exports, "UserRoutes", { enumerable: true, get: function () { return student_route_1.UserRoutes; } });
